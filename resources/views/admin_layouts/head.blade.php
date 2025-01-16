c81e6ab5e9daf44457ff6d6090f32bc60fc014cc
sophia <thangneimawi@hivelocity.co.jp>
2025-01-16 20:48:41 +0700

    fixed css

--- resources/views/admin_layouts/head.blade.php
+++ resources/views/admin_layouts/head.blade.php
@@ -22,6 +22,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <!-- CSS Files -->
     <link id="pagestyle" href="{{ asset('admin_app/assets/css/material-dashboard.css?v=3.0.6') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{asset('admin_app/assets/css/flatpickr.min.css')}}">
 
     <!-- Nepcha Analytics (nepcha.com) -->
     <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
