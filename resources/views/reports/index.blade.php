117cd2bdeb81ae41b8cfd36273fffb48dbee2116
AMK1215 <aungmyokhaing1215@gmail.com>
2025-01-29 09:26:13 +0630

    bet api updated by amk

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -82,8 +82,13 @@
                                     <td> <span
                                             class="{{ $profit > 1 ? 'text-success' : 'text-danger' }}">{{ number_format($profit, 2) }}</span>
                                     </td>
-                                    <td><a href="{{ route('admin.reportv1.detail', $result->user_id) }}"
-                                            class="btn btn-primary">Detail</a></td>
+                                    {{-- <td><a href="{{ route('admin.reportv1.detail', $result->user_id) }}"
+                                            class="btn btn-primary">Detail</a></td> --}}
+                                    <td>
+                                        <a href="{{ route('admin.report.reportv1.detail', $result->user_id) }}"
+                                            class="btn btn-primary">Detail</a>
+
+                                    </td>
                                 </tr>
                             @endforeach
 
